c2fd34b741a8f9fab9f3014c422f3c49f128c73a
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2017-06-06 18:16:00 +0200

    Add unit-tests for UniqueSessionIdGenerator in SessionMiddlewareDefaults

---
+++ tests/OwinSessionMiddlewareTests/SessionMiddlewareDefaultsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OwinSessionMiddleware;
+
+namespace OwinSessionMiddlewareTests
+{
+    [TestFixture]
+    public class SessionMiddlewareDefaultsTests
+    {
+        [Test]
+        public void UniqueSessionIdGenerator_CallThousandTimes_ValuesShouldBeUnique()
+        {
+            var set = new HashSet<string>(Enumerable.Range(0, 1000).Select(_ => SessionMiddlewareDefaults.UniqueSessionIdGenerator()));
+            Assert.That(set.Count, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void UniqueSessionIdGenerator_CallThousandTimes_ResultShouldNotExceed45Characters()
+        {
+            var maxLength = Enumerable.Range(0, 1000).Max(_ => SessionMiddlewareDefaults.UniqueSessionIdGenerator().Length);
+            Assert.That(maxLength, Is.LessThanOrEqualTo(45));
+            Assert.That(maxLength, Is.GreaterThan(40), "Max length too small, consider re-running the test");
+        }
+    }
+}
